96180a1f1452744808bb9baf1233487684884216
Angel Jodra <ajodra09@gmail.com>
2020-01-31 23:45:39 +0100

    =Final de la sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,29 +9,43 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
  
   constructor(private http: HttpClient) {
 
-    console.log("Servicio inPagina Listo");
-  
-    //Leer JSON
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe ( resp => {
-
-        //console.log (resp);
-        //console.log (resp['twitter']);
-        //console.log (resp['email']);
-        this.cargada = true;
-        this.info = resp;
-
-        console.log (resp);
-
-      } 
-        
-      
-    )
+    //console.log("Servicio inPagina Listo");
+    this.cargarInfo();
+    this.cargarEquipo();
 
   }
- 
+
+  //Cargar los datos de JSON
+  private cargarInfo() {
+
+    //Leer JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe ( (resp: InfoPagina) => {
+
+        this.cargada = true;
+        this.info = resp;
+        //console.log(resp)
+      });
+  }
+
+  //Cargar los datos de Firebase
+  private cargarEquipo() {
+    //Leer JSON
+    this.http.get('https://angular-html-16733.firebaseio.com/equipo.json')
+      .subscribe ( (resp: any []) => {
+
+        this.equipo = resp;
+        console.log(resp)
+      });
+
+  }
+
+
+
+
 }
 
